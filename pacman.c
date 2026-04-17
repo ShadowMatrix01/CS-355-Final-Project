78c02d3983533f9cc6c6de4117a086c6ed790e88
Jhan Gomez <jhangomez971@gmail.com>
2026-04-17 19:37:44 -0400

    Added dots, defined elements, color defined, and other key mechanics

--- pacman.c
+++ pacman.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <ncurses.h> //Ncurses library ported in
 #include <stdbool.h>
+#include <time.h> //Time to handle start screen. https://stackoverflow.com/questions/17167949/how-can-i-use-a-timer-in-c 
+#include <stdlib.h> //Needed for exit.
+//Splash screen might be added later.
 #define WIDTH 60 //Because it looked incorrect
 #define HEIGHT 30 
-
-// Width and height from: https://tldp.org/HOWTO/NCURSES-Programming-HOWTO/keys.html
-/*Pac-Man
+#define WALL '#' //Needed for collision purposes.
+#define DOT '.' //Needed to populate the dots.
+#define SPECIAL '@'
+#define CHERRY 'C'
+#define EATEN ' '
+/*  Width and height from: https://tldp.org/HOWTO/NCURSES-Programming-HOWTO/keys.html
+ Pac-Man
 • Objective: navigate a maze and eat all Pac-Dots.
 • Game Design:
 – The maze must be 30 → 30 characters in size.
@@ -17,34 +24,97 @@ time.
 the ability to eat the ghosts for 10 seconds.
 – The maze contains 150 Pac-Dots.
 • Game Play: The game ends with a win if Pac-Man eats all the Pac-Dots. The game ends in
-lost if Pac-Man is eaten by a ghost*/
+lost if Pac-Man is eaten by a ghost
 
-int pacman_x = 30;
-int pacman_y = 15;
-int main(int argc, char * argv[]) {
+https://pacman.fandom.com/wiki/Fruits
+The fruits feature different mechanics in certain games, but across all of them, they can appear twice in every round.
+In the original Pac-Man, the fruits are located below the Regeneration Chamber and won't move at all. 
+The fruit appears after 70 dots are eaten and again after 170 dots are eaten unless the first fruit is still there. They will disappear if they are not eaten after 9-10 seconds.*/
+
+int pacman_x = 1;
+int pacman_y = 1;
+int main(int argc, char * argv[]) { 
+char stage[HEIGHT][WIDTH + 1] = { //WIDTH + 1 because of the C null terminator, which is why this was wreaking havoc LOL!
+    "############################################################", 
+    "#..........................................................#", 
+    "#..........................................................#",
+    "#..........................................................#",
+    "#..........................................................#",
+    "#..........................................................#",
+    "#..........................................................#",
+    "#..........................................................#",
+    "#..........................................................#",
+    "#..........................................................#",
+    "#..........................................................#",
+    "#..........................................................#",
+    "#..........................................................#",
+    "#..........................................................#",
+    "#..........................................................#",
+    "#..........................................................#",
+    "#..........................................................#",
+    "#..........................................................#",
+    "#..........................................................#",
+    "#..........................................................#",
+    "#..........................................................#",
+    "#..........................................................#",
+    "#..........................................................#",
+    "#..........................................................#",
+    "#..........................................................#",
+    "#..........................................................#",
+    "#..........................................................#",
+    "#..........................................................#",
+    "#..........................................................#", 
+    "############################################################"  
+};						
+								
    WINDOW *game_win;
    int pacman_move;
-   int direction = KEY_LEFT; //https://pacmancode.com/start-positions
+   int direction = KEY_RIGHT; //https://pacmancode.com/start-positions
    bool running = true;
    initscr();
+   start_color(); //Needed to initialize color pairs. 
+   init_pair(1, COLOR_YELLOW, COLOR_BLACK); //Key 1, Yellow Character, Black Background.
+   init_pair(2, COLOR_BLUE, COLOR_BLACK);   //Key 2, Blue Character, Black Background.
+   init_pair(3, COLOR_CYAN, COLOR_BLACK); //Key 3, Magenta Character, Black Background.
+   init_pair(4, COLOR_RED, COLOR_BLACK);    //Key 4, Red Character, Black Background.
+   init_pair(5, COLOR_GREEN, COLOR_BLACK); //Key 5, Green Character, Black Background.
+   
+   //NOTE: While in a normal pacman game, the dots would be yellow. I dislike how much the yellow blends
+   //with pacman because you cannot change the opacity of a color in ncurses. Or if is, it adds unnecessary bloat.
+   if(has_colors() == FALSE){	
+	    endwin(); //Handles if color is not supported on the terminal.
+		printf("Your terminal does not support color\n");
+		exit(1);
+   } //https://tldp.org/HOWTO/NCURSES-Programming-HOWTO/color.html
    clear();
    noecho();
    cbreak();
    keypad(stdscr, TRUE);
-   game_win = newwin(HEIGHT, WIDTH, 0, 0); //Window initialized with
-   //size 60 x 30. See comment for reason.
-   box(game_win, 0, 0); //Border around the screen.
+   game_win = newwin(HEIGHT, WIDTH, 0, 0); //Window initialized with size 60 x 30. See comment for reason.
   
    curs_set(0); //Cursor hidden from terminal, because that breaks game flow.
-   refresh(); //Stdscr refreshed
    wrefresh(game_win); //Game window refreshed.
    nodelay(stdscr, TRUE); //Had to add this, because input is normally blocking.
    
    while (running) {
+    werase(game_win); //Game window is cleared of previous screen.
     mvwaddch(game_win, pacman_y, pacman_x, ' '); //Needed to clear trailing output, similar to pygame.
+    for (int y = 0; y < HEIGHT; y++) { //This loop draws the stage as it evolves. Starting with the y axis, or the columns.
+		for (int x = 0; x < WIDTH; x++) { //Same as above, but for x axis
+			if (stage[y][x] == WALL) {
+			    mvwaddch(game_win, y, x, stage[y][x] | COLOR_PAIR(2)); //Stage drawn.
+			} else if (stage[y][x] ==  DOT) {
+				mvwaddch(game_win, y, x, stage[y][x] | COLOR_PAIR(3)); //Stage drawn.	
+			} else if (stage[y][x] == CHERRY) {
+				mvwaddch(game_win, y, x, stage[y][x] | COLOR_PAIR(4)); //Stage drawn.	
+			} else if (stage[y][x] == SPECIAL) {
+				mvwaddch(game_win, y, x, stage[y][x] | COLOR_PAIR(5)); //Stage drawn.	
+			} else {
+				mvwaddch(game_win, y, x, stage[y][x]); //Stage drawn.
+			}
+		}
+	}
     pacman_move = getch();
-    move(30,0);
-    clrtoeol(); //This is how to avoid the weird line artifacting appparently.
     switch(pacman_move) {
         case KEY_UP: 
         case KEY_DOWN:
@@ -58,7 +128,7 @@ int main(int argc, char * argv[]) {
     }
     switch(direction) {
         case KEY_UP: 
-             if (pacman_y > 1) { //Needed to avoid an out of bounds bug.
+             if (pacman_y > 1 && stage[pacman_y][pacman_x]!=WALL) { //Needed to avoid an out of bounds bug.
                 pacman_y--; //Pacman moves up by decrementing y axis by one.
              }
              break;
@@ -78,8 +148,8 @@ int main(int argc, char * argv[]) {
              }
              break;
     }
-    mvwaddch(game_win, pacman_y, pacman_x, 'P'); //https://docs.oracle.com/cd/E86824_01/html/E54767/mvwaddch-3curses.html
-    refresh(); //Stdscr refreshed
+    
+    mvwaddch(game_win, pacman_y, pacman_x, 'P' | COLOR_PAIR(1)); //https://docs.oracle.com/cd/E86824_01/html/E54767/mvwaddch-3curses.html
     wrefresh(game_win); //Game window refreshed.
     napms(100); 
    }
@@ -88,3 +158,4 @@ int main(int argc, char * argv[]) {
 }
 
 //Note, to compile gcc -o pacman pacman.c -lncurses
+
