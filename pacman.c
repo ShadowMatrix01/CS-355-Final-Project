2c199a1e904cb5ebeada6d3f62f3ba94e57e1590
Jhan Gomez <jhangomez971@gmail.com>
2026-04-24 15:15:06 -0400

    Reworked logic, changed maze, pacman tunnel functionality added, colors corrected, and cherry implemented.

--- pacman.c
+++ pacman.c
@@ -2,11 +2,12 @@
 #include <ncurses.h> //Ncurses library ported in
 #include <stdbool.h>
 #include <time.h> //Time to handle start screen. https://stackoverflow.com/questions/17167949/how-can-i-use-a-timer-in-c 
-#include <stdlib.h> //Needed for exit.
+#include <stdlib.h> //Needed for exit and random.
 //Splash screen might be added later.
 #define WIDTH 60 //Because it looked incorrect
 #define HEIGHT 30 
 #define WALL '#' //Needed for collision purposes.
+#define WALL2 '-'//Same reason as above, to make the ghost logic easier.
 #define DOT '.' //Needed to populate the dots.
 #define SPECIAL '@'
 #define CHERRY 'C'
@@ -45,6 +46,13 @@ int next_direction_x=1;
 int next_direction_y=1;
 int score = 0;
 int lives = 3;
+int cherry_end_time = 0;
+int seconds = 10; //For cherry timer.
+bool going_once = true;
+bool start_timer_cherry = false;
+void ghost_time() {
+	//Will need timer, aswell as cool effect and other things.
+}
 int main(int argc, char * argv[]) { 
    char stage[HEIGHT][WIDTH + 1] = { 
     "############################################################", 
@@ -60,11 +68,11 @@ int main(int argc, char * argv[]) {
     "           ##  ###########  ####  ###########  ##           ",
     "           ##  ###########  ####  ###########  ##           ",
     "           ##  #                            #  ##           ",
-    "#############  #                            #  #############",
-    "                                                            ",
-    "#############  #                            #  #############",
-    "           ##  #                            #  ##           ",
+    "#############  #  ##########----##########  #  #############",
+    "                  #                      #                  ",
+    "#############  #  #                      #  #  #############",
     "           ##  #  ########################  #  ##           ",
+    "           ##  #                            #  ##           ",
     "           ##  #  ########################  #  ##           ",
     "#############  #  ########################  #  #############",
     "#.........      .           ####           .     ..........#",
@@ -88,11 +96,14 @@ int main(int argc, char * argv[]) {
    bool running = true;
    initscr();
    start_color(); //Needed to initialize color pairs. 
-   init_pair(1, COLOR_YELLOW, COLOR_BLACK); //Key 1, Yellow Character, Black Background.
-   init_pair(2, COLOR_BLUE, COLOR_BLACK);   //Key 2, Blue Character, Black Background.
-   init_pair(3, COLOR_CYAN, COLOR_BLACK); //Key 3, Magenta Character, Black Background.
-   init_pair(4, COLOR_RED, COLOR_BLACK);    //Key 4, Red Character, Black Background.
-   init_pair(5, COLOR_GREEN, COLOR_BLACK); //Key 5, Green Character, Black Background.
+   //Needed for codespaces to not highlight text or show grey as the background
+   use_default_colors();
+   init_pair(1, COLOR_GREEN, -1); //Key 1, Green Character, Black Background.
+   init_pair(1, COLOR_YELLOW, -1); //Key 1, Yellow Character, Black Background.
+   init_pair(2, COLOR_BLUE, -1);   //Key 2, Blue Character, Black Background.
+   init_pair(3, COLOR_CYAN, -1); //Key 3, Magenta Character, Black Background.
+   init_pair(4, COLOR_RED, -1);    //Key 4, Red Character, Black Background.
+   init_pair(5, COLOR_GREEN, -1); //Key 5, Green Character, Black Background.
    
    //NOTE: While in a normal pacman game, the dots would be yellow. I dislike how much the yellow blends
    //with pacman because you cannot change the opacity of a color in ncurses. Or if is, it adds unnecessary bloat.
@@ -115,11 +126,23 @@ int main(int argc, char * argv[]) {
    nodelay(game_win, TRUE);//Had to add this, because input is normally blocking.
    
    while (running) {
-	mvprintw(8, 90, "SCORE: %d LIVES: %d", score, lives); //https://tldp.org/HOWTO/NCURSES-Programming-HOWTO/printw.html
+	mvprintw(8, 90, "SCORE: %d                                           LIVES: %d", score, lives); //https://tldp.org/HOWTO/NCURSES-Programming-HOWTO/printw.html
 	refresh();
     werase(game_win); //Game window is cleared of previous screen.
     mvwaddch(game_win, pacman_y, pacman_x, ' '); //Needed to clear trailing output, similar to pygame.
-    
+    //Timer is also like pygame, I couldn't use sleep since sleep blocks the CPU.
+    if (start_timer_cherry) {
+		if (time(NULL) >= cherry_end_time) {
+			stage[17][29] = EATEN;
+			start_timer_cherry = false;
+		}
+	}
+    if (score > 70 && going_once) {
+		stage[17][29] = CHERRY;
+		start_timer_cherry = true;
+		going_once = false;
+		cherry_end_time = time(NULL) + 10; //Takes the current time and adds 10 seconds, like in the original game.
+	}
     for (int y = 0; y < HEIGHT; y++) { //This loop draws the stage as it evolves. Starting with the y axis, or the columns.
 		for (int x = 0; x < WIDTH; x++) { //Same as above, but for x axis
 			if (stage[y][x] == WALL) {
@@ -163,16 +186,35 @@ int main(int argc, char * argv[]) {
     }
     //Prevents an out of bound error for the program.
     if (next_direction_y >= 0 && next_direction_y < HEIGHT &&  next_direction_x >= 0 && next_direction_x < WIDTH) {
-		if (stage[next_direction_y][next_direction_x] != WALL) { //This will only update pacman x and pacman y if its not a wall.
+		if ((stage[next_direction_y][next_direction_x] != WALL) && (stage[next_direction_y][next_direction_x] != WALL2)) { //This will only update pacman x and pacman y if its not a wall.
             pacman_y = next_direction_y;
             pacman_x = next_direction_x;
 		}
-		if (stage[next_direction_y][next_direction_x] == DOT) {
+		if (stage[next_direction_y][next_direction_x] == DOT || stage[next_direction_y][next_direction_x] == CHERRY || stage[next_direction_y][next_direction_x] == SPECIAL) {
+			if (stage[next_direction_y][next_direction_x] == DOT) {
+				score+=10;
+			} else if (stage[next_direction_y][next_direction_x] == CHERRY) {
+				score+=100;
+			} else {
+				ghost_time();
+			}
 			stage[next_direction_y][next_direction_x] = EATEN;
-			score+=10;
 		}
 		
 	}
+	//This is how pacman can move through the tunnel and teleport.
+	if (next_direction_y == 14) {
+		if (next_direction_x < 0) { 
+			pacman_x = WIDTH - 1;
+			pacman_y = 14; 
+			direction = KEY_LEFT;
+    } else if (next_direction_x >= WIDTH) { 
+        pacman_x = 0;
+        pacman_y = 14;
+        direction = KEY_RIGHT;
+    }
+	}
+	
     mvwaddch(game_win, pacman_y, pacman_x, 'P' | COLOR_PAIR(1)); //https://docs.oracle.com/cd/E86824_01/html/E54767/mvwaddch-3curses.html
     wrefresh(game_win); //Game window refreshed.
     napms(100); 
